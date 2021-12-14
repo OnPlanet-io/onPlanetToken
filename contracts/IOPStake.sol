9a376ca9bc777a67e54ac86be3754685d72a3762
cartoonweber <cartonweber0426@gmail.com>
2021-12-15 00:02:29 +0800

    Remove unused files

--- contracts/IOPStake.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.8.4;
-
-interface IOPStake {
-    function createRewards(address acount, uint256 tAmount) external;
-
-    function deliver(uint256 tAmount) external;
-}
